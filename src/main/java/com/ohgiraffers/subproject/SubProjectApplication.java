9e07091965dc6483c62d643c26b7b5be342f6948
spehil <boo1231@naver.com>
2023-09-25 09:24:59 +0900

    (project init) test commit

---
+++ src/main/java/com/ohgiraffers/subproject/SubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.subproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/subproject/SubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.subproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
