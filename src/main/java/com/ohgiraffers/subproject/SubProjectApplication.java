c8bcbca01b34fdd7a2ff91a05e1ae1ec6898e90d
spehil <boo1231@naver.com>
2023-09-25 10:02:48 +0900

    (project init) final commit

--- src/main/java/com/ohgiraffers/subproject/SubProjectApplication.java
+++ src/main/java/com/ohgiraffers/subproject/SubProjectApplication.java
@@ -8,7 +8,7 @@ public class SubProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubProjectApplication.class, args);
-        System.out.println("ㅎㅎㅎ");
+        System.out.println("ㅎㅎㅎㅎㅎ");
     }
 
 }
